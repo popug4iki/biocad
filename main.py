75a6bba2637d397b4e8fe6dd8844498b3f6b76ca
PELENIUM <i.d.chernyshov@yandex.ru>
2024-10-27 16:35:47 +0300

    added flask server

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-from langdetect import detect
-print()
-print(detect('\ncerebral palsy is a nonprogressive central'))
+from flask import Flask, render_template
+import transformers
+import torch
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = transformers.AutoModelForSeq2SeqLM.from_pretrained("./exp/model").to(device)
+tokenizer = transformers.AutoTokenizer.from_pretrained("./exp/tokenizer")
+
+
+def chunk_text(text, tokenizer, max_length=512):
+    tokens = tokenizer.encode(text)
+    return [tokens[i:i + max_length] for i in range(0, len(tokens), max_length)]
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# @app.route('')
+# def api():
+#     pass
+
+
+app.run(host='0.0.0.0', debug=True)
